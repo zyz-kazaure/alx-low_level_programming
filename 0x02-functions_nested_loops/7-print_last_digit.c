ccf923b9aa1fc973c23f7af5e776041bb11992ee
zyz-kazaure <zyzkazaure@gmail.com>
2023-03-21 22:05:53 +0000

    dhdj

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - output last digit of a number
+ * @n : number to check
+ *
+ * Return: number
+ */
+int print_last_digit(int n)
+{
+	int result;
+
+	result = n % 10;
+	if (n < 0)
+		result *= -1;
+	_putchar(result + '0');
+	return (result);
+}
