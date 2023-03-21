c727a77a3d6516d476c81d8cf9ed192b0dd58b93
zyz-kazaure <zyzkazaure@gmail.com>
2023-03-21 22:59:04 +0000

    dndnd

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * print_times_table - outp
+ * @n : specified
+ */
+void print_times_table(int n)
+{
+	int product, bigger, i, j;
+
+	if ((n >= 0) && (n <= 15))
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(48);
+			for (j = 1; j <= n; j++)
+			{
+				product = i * j;
+				_putchar(',');
+				_putchar(' ');
+				if (product < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 100)
+				{
+					bigger = product / 10;
+					_putchar(bigger / 10 + '0');
+					_putchar(bigger % 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(product / 10 + '0');
+					_putchar(product % 10 + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
