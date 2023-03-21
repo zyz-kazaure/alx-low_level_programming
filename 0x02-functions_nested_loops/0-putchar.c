85a8038bda29ca0b167570d38ee7a96e3e6d51ec
zyz-kazaure <zyzkazaure@gmail.com>
2023-03-21 15:04:27 +0000

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - starting the fuction
+ *
+ * _putchar: is for printing characters
+ *
+ * Return: the end of the fuction
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+
+}
