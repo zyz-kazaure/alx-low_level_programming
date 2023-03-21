9bdb462289d12e9ed2b961c64def44a279fadd23
zyz-kazaure <zyzkazaure@gmail.com>
2023-03-21 21:47:40 +0000

    ddjdj

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *print_sign - Evaluate sign of number
+ *@n: number to be checked
+ *
+ * Return: 1, 0 or -1
+ */
+int print_sign(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		_putchar('+');
+		test = 1;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		test = 0;
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		test = -1;
+	}
+	return (test);
+}
