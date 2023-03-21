6af66645703902eff41bc699d39dae2fcf5898f0
zyz-kazaure <zyzkazaure@gmail.com>
2023-03-21 23:22:23 +0000

    sdksks

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *Desc: program that prints the first 50 Fibonacci
+ *numbers, starting with 1 and 2
+ *
+ *Return: Always 0
+ */
+int main(void)
+{
+	long int i, prev, next, ans;
+
+	prev = 1;
+	next = 1;
+	ans = prev + next;
+	printf("%ld, %ld, ", next, ans);
+	for (i = 1; i <= 48; i++)
+	{
+		prev = next;
+		next = ans;
+		ans = prev + next;
+		if (i == 48)																			printf("%ld\n", ans);																else																				printf("%ld, ", ans);																				}
+	return (0);
+}
+
+
