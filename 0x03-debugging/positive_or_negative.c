0d68e92f0f0e172e13ae86ae9053d87f7a84c37c
zyz-kazaure <zyzkazaure@gmail.com>
2023-03-22 13:23:45 +0000

    djjsjs

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * positive_or_negative - ndjjd
+ *
+ * @i: djdjdjd
+ *
+ * Return: djdjsdk
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is positive", i);
+}
